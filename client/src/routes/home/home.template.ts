8915aac4be37ecd6d7545a08d15d72a58db9cc42
Matt Walker <matt.walker@genesis.global>
2024-03-14 15:36:04 +0000

    remove ui schema

--- client/src/routes/home/home.template.ts
+++ client/src/routes/home/home.template.ts
@@ -16,16 +16,6 @@ export const HomeTemplate = html<Home>`
       <zero-layout-item title="two">
         <foundation-form
           resourceName="EVENT_AMEND_USER"
-          :uischema="${(x) => ({
-    type: 'VerticalLayout',
-    elements: [
-      { type: 'Control', scope: '#/properties/USER_PROFILES' },
-      { type: 'Control', scope: '#/properties/ACCESS_TYPE' },
-      { type: 'Control', scope: '#/properties/USER_NAME' },
-      { type: 'Control', scope: '#/properties/USER_TYPE' },
-      { type: 'Control', scope: '#/properties/WEBSITE' },
-    ],
-  })}"
         ></foundation-form>
       </zero-layout-item>
     </zero-layout-region>
